dda48eadb67615fddfaa5726c975054f8f2f41f4
Jepson <patryk.pelczar@gmail.com>
2020-05-07 00:14:34 +0200

    Add files via upload

--- gtranslator.js
+++ gtranslator.js
@@ -13,18 +13,25 @@ module.exports = async function gt (content = {answers: ['0'], q: '0'}) {
         })
         result.q = {original: result.q, translated: ``}
         let comps = [...result.answers, result.q]
-
+        
+        let ac = 1
         for (let x = 0; x < comps.length; x++) {
             if (comps[x].original !== `*image*`) {
-                const inElem = await operationCenter.findElement(By.css('.tlid-source-text-input'))
-                inElem.clear()
-                inElem.sendKeys(comps[x].original)
-                const translationBy = By.css(`.tlid-translation`)
-                const translationElem = await operationCenter.wait(until.elementLocated(translationBy), 4000);
-                comps[x].translated = await translationElem.getText()
-                await operationCenter.findElement(By.css('.tlid-clear-source-text')).click()
+                if (x > 0 && comps[x].original == comps[x-ac].original) {
+                    comps[x].translated = comps[x-ac].translated
+                    ac = 1
+                } else {
+                    const inElem = await operationCenter.findElement(By.css('.tlid-source-text-input'))
+                    inElem.clear()
+                    inElem.sendKeys(comps[x].original)
+                    const translationBy = By.css(`.tlid-translation`)
+                    const translationElem = await operationCenter.wait(until.elementLocated(translationBy), 8000);
+                    comps[x].translated = await translationElem.getText()
+                    await operationCenter.findElement(By.css('.tlid-clear-source-text')).click()
+                }
             } else {
                 comps[x].translated = `*image*`
+                ac++
             }
         }
         return result
